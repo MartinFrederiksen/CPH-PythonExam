f4fa5ccd7f35ee8785aa3257a0b7911b32bbe03d
MartinFrederiksen <zyjinxx@hotmail.com>
2020-05-19 15:38:41 +0200

    Update

--- TestNew.py
+++ TestNew.py
@@ -27,14 +27,16 @@ from keras.preprocessing import image
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
+# Normalize the x_train and x_test to be a float between 0 and 1
 x_train = x_train / 255.0
 x_test = x_test / 255.0
 
-nClasses = 10
-y_train = np_utils.to_categorical(y_train, nClasses)
-y_test = np_utils.to_categorical(y_test, nClasses)
+# One-hot encoding based on number of classes
+class_count = 10
+y_train = np_utils.to_categorical(y_train, class_count)
+y_test = np_utils.to_categorical(y_test, class_count)
 
-checkpoint_path = "training/cp_v2.ckpt"
+checkpoint_path = "training/cp_v1.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 
@@ -53,8 +55,8 @@ def create_model():
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
 
-    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
     
@@ -63,7 +65,7 @@ def create_model():
     # Dense NN
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(nClasses, activation='softmax'))
+    model.add(Dense(class_count, activation='softmax'))
 
 
     # Optimizer loss - klassifikation
@@ -80,6 +82,9 @@ def fit_model(model):
             validation_data=(x_test, y_test),
             callbacks=[cp_callback])  # Pass callback to training
     
+    # Saves the model
+    model.save('training/cifar10_model_{}.h5'.format(checkpoint_path.split('_')[1].split('.')[0]))
+    
     return history
 
 
@@ -91,13 +96,13 @@ def eval_test():
     loss, acc = model.evaluate(x_test,  y_test, verbose=2)
     print("Untrained model, accuracy: {:5.2f}%".format(100*acc))
 
-
     # Loads the weights
     model.load_weights(checkpoint_path)
 
     # Re-evaluate the model
     loss,acc = model.evaluate(x_test,  y_test, verbose=2)
     print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
 
 def make_plots(history):
     # Loss curve
@@ -110,7 +115,6 @@ def make_plots(history):
     plt.title('Loss curve', fontsize=16)
     plt.savefig('figures/loss.png', bbox_inches='tight', dpi=300)
 
-
     # Accuracy curve
     plt.figure(figsize=[8, 6])
     plt.plot(history.history['accuracy'], 'black', linewidth=3.0)
@@ -122,30 +126,37 @@ def make_plots(history):
     plt.savefig('figures/accuracy.png', bbox_inches='tight', dpi=300)
 
 
-# Create a basic model instance
-model = create_model()
+def sort_unknown():
+    loaded_model = load_model('training/cifar10_model_v1.h5')
+    files = os.listdir('images/model_test')
+    for file in files:
+        img = image.load_img(os.path.join('images/model_test', file), target_size=(32, 32))
+        img = np.expand_dims(img, axis=0)
+        result=loaded_model.predict_classes(img)
+        print('File: {} -- Prediction: {}'.format(file, class_names[result[0]]))
+
+
+## Create a basic model instance
+#model = create_model()
 
 ## Display the model's architecture
 # model.summary()
 
-# Fit the model
-history = fit_model(model)
-make_plots(history)
+## Fit the model
+#history = fit_model(model)
 
-# Load the weights and evaluate the model
-# model.load_weights(checkpoint_path)
-loss, acc = model.evaluate(x_test,  y_test, verbose=2)
-print("Restored model, accuracy: {:5.2f}%".format(100*acc))
-# print(model.input_names)
+## Make the plots
+#make_plots(history)
 
+## Load the weights from a saved model
 #model.load_weights(checkpoint_path)
-model.save('training/cifar10_model_v2.h5')
 
-loaded_model = load_model('training/cifar10_model_v1.h5')
-#loaded_model.layers[0].input_shape #(None, 32, 32, 3)
-image_path='images/SquareDoggo.jpg'
-IMG_SIZE = 32
-img = image.load_img(image_path, target_size=(IMG_SIZE, IMG_SIZE))
-img = np.expand_dims(img, axis=0)
-result=loaded_model.predict_classes(img)
-print(class_names[result[0]])
+
+# loaded_model = load_model('training/cifar10_model_v1.h5')
+# #loaded_model.layers[0].input_shape #(None, 32, 32, 3)
+# image_path='images/model_test/SquareDoggo.jpg'
+# IMG_SIZE = 32
+# img = image.load_img(image_path, target_size=(IMG_SIZE, IMG_SIZE))
+# img = np.expand_dims(img, axis=0)
+# result=loaded_model.predict_classes(img)
+# print(class_names[result[0]])
